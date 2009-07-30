b0b23ef892b0133dfd83a0cf9749ed00aa2c3e50
wu <wu@localhost.(none)>
2009-07-31 01:15:23 +0200

    modified:   stempel/stempel.txt 	modified:   tools/tester.cpp

--- tools/tester.cpp
+++ tools/tester.cpp
@@ -19,6 +19,7 @@ int main(int argc, char *argv[], char *env[])
 {
 	IplImage *src;
 	struct intern_bitmap *bm;
+	int i;
 
 	/* Argumente testen */
 	if (argc == 1) {
@@ -29,24 +30,25 @@ int main(int argc, char *argv[], char *env[])
 	/* init OpenCV */
 	cvInitSystem(argc, argv);
 	
-	/* Bild Lesen */
-	src = cvLoadImage(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
-	if (!src) {
-		printf("Error: cvLoadImage()\n");
-		return 1;
+	for (i = 1; i < argc; i++ ) {
+		/* Bild Lesen */
+		src = cvLoadImage(argv[i], CV_LOAD_IMAGE_GRAYSCALE);
+		if (!src) {
+			printf("Error: cvLoadImage()\n");
+			return 1;
+		}
+		
+		bm = preprocess(src);
+		ocr_bestpassend(bm, ergebnis, ERGEBNIS_LAENGE);
+		bm_release(bm);
+		
+		printf("Ergebnis: %s\n", ergebnis);
+		
+		/* das original Bild anzeigen */
+		cvNamedWindow("Demo Window", CV_WINDOW_AUTOSIZE);
+		cvShowImage("Demo Window", src);
+		cvWaitKey(-1);
+		cvDestroyWindow("Demo Window");
 	}
-	
-	bm = preprocess(src);
-	ocr_bestpassend(bm, ergebnis, ERGEBNIS_LAENGE);
-	bm_release(bm);
-	
-	printf("Ergebnis: %s\n", ergebnis);
-	
-	/* das original Bild anzeigen */
-	cvNamedWindow("Demo Window", CV_WINDOW_AUTOSIZE);
-	cvShowImage("Demo Window", src);
-	cvWaitKey(-1);
-	cvDestroyWindow("Demo Window");
-
 	return 0;
 }
