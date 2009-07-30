4b1ea5b43bb6b5756f2877b52c99a84af96d9f67
wu <wu@localhost.(none)>
2009-07-31 00:03:34 +0200

    modified:   tools/stempel_txt2c.pl

--- tools/stempel_txt2c.pl
+++ tools/stempel_txt2c.pl
@@ -2,18 +2,20 @@
 use strict;
 # annehmen, dass die eingangsdaten auf jedem Fall richtig ist
 
-print "\nstruct zeichen_vektor daten_muster = {\n";
+print "\nstruct zeichen_vektor daten_muster[] = {\n";
 
-our ($name, $val);
+our ($name, $val, $i);
 
 $name = <>;
 chomp($name);
+$i = 0;
 
 while (1){
 	$val = <>;
 	chomp($val);
 	$val =~ s/ /,/g;
 	print '{"', $name, '", {', $val, "}";
+	$i++;
 	$name = <>;
 	if (defined($name)) {
 		print ",\n";
@@ -24,4 +26,6 @@ while (1){
 	chomp($name);
 }
 
-print "};";
+print "};\n";
+
+print "#define ZEICHEN_MUSTER_MENGE $i\n";
